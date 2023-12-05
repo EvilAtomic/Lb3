c16773328ca45711afc4a6a765af15abb95a5f67
{{EvilAtomic}} <{{artemasarkisan598@gmail.com}}>
2023-12-05 10:49:02 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+class Point
+{
+    // Числові та рядкове поля
+    private double x;
+    private double y;
+    private string name;
+
+    // Конструктор класу Point
+    public Point(double x, double y, string name)
+    {
+        this.x = x;
+        this.y = y;
+        this.name = name;
+    }
+
+    // Властивості для отримання значень полів
+    public double X
+    {
+        get { return x; }
+    }
+
+    public double Y
+    {
+        get { return y; }
+    }
+
+    public string Name
+    {
+        get { return name; }
+    }
+}
+
+class Figure
+{
+    private Point[] points;
+
+    // Конструктор класу Figure для різної кількості точок
+    public Figure(Point point1, Point point2, Point point3)
+    {
+        points = new Point[] { point1, point2, point3 };
+    }
+
+    public Figure(Point point1, Point point2, Point point3, Point point4)
+    {
+        points = new Point[] { point1, point2, point3, point4 };
+    }
+
+    public Figure(Point point1, Point point2, Point point3, Point point4, Point point5)
+    {
+        points = new Point[] { point1, point2, point3, point4, point5 };
+    }
+
+    // Метод для розрахунку довжини сторони багатокутника
+    private double GetSideLength(Point A, Point B)
+    {
+        return Math.Sqrt(Math.Pow(B.X - A.X, 2) + Math.Pow(B.Y - A.Y, 2));
+    }
+
+    // Метод для розрахунку периметра багатокутника
+    public void CalculatePerimeter()
+    {
+        double perimeter = 0;
+        for (int i = 0; i < points.Length - 1; i++)
+        {
+            perimeter += GetSideLength(points[i], points[i + 1]);
+        }
+        perimeter += GetSideLength(points[points.Length - 1], points[0]);
+
+        Console.WriteLine($"Назва багатокутника: {points.Length}-кут");
+        Console.WriteLine($"Периметр багатокутника: {perimeter}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Створюємо об'єкти класу Point для точок
+        Point point1 = new Point(0, 0, "A");
+        Point point2 = new Point(0, 1, "B");
+        Point point3 = new Point(1, 1, "C");
+
+        // Створюємо об'єкт класу Figure і викликаємо метод CalculatePerimeter
+        Figure triangle = new Figure(point1, point2, point3);
+        triangle.CalculatePerimeter();
+
+        Console.ReadLine(); // Затримка консолі
+    }
+}
---
+++ Lb3/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+class Rectangle
+{
+    // Поля класу
+    private double side1;
+    private double side2;
+
+    // Конструктор класу
+    public Rectangle(double side1, double side2)
+    {
+        this.side1 = side1;
+        this.side2 = side2;
+    }
+
+    // Метод для обчислення площі прямокутника
+    public double CalculateArea()
+    {
+        return side1 * side2;
+    }
+
+    // Метод для обчислення периметра прямокутника
+    public double CalculatePerimeter()
+    {
+        return 2 * (side1 + side2);
+    }
+
+    // Властивість для отримання площі прямокутника
+    public double Area
+    {
+        get { return CalculateArea(); }
+    }
+
+    // Властивість для отримання периметра прямокутника
+    public double Perimeter
+    {
+        get { return CalculatePerimeter(); }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Зчитуємо довжини сторін від користувача
+        Console.Write("Введіть довжину сторони 1: ");
+        double side1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Введіть довжину сторони 2: ");
+        double side2 = double.Parse(Console.ReadLine());
+
+        // Створюємо об'єкт класу Rectangle
+        Rectangle rectangle = new Rectangle(side1, side2);
+
+        // Виводимо периметр та площу на екран
+        Console.WriteLine($"Периметр прямокутника: {rectangle.Perimeter}");
+        Console.WriteLine($"Площа прямокутника: {rectangle.Area}");
+
+        Console.ReadLine(); // Затримка консолі
+    }
+}
